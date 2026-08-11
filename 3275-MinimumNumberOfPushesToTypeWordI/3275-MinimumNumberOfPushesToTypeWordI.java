19092a1ad06e7fbe85594a2a9291522e9d5cbe88
Lingeshbs-2007 <lingeshbs2007@gmail.com>
2026-08-11 20:44:47 +0530

    Create 3275-MinimumNumberOfPushesToTypeWordI

---
+++ 3275-MinimumNumberOfPushesToTypeWordI/3275-MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 8:44:45 PM
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        if( n <= 8){
+            return n;
+        }
+        else if( n > 8 && n <= 16){
+            return 8 +(n-8)*2;
+        }
+        else if( n > 16 && n <= 24){
+            return 8+(8*2)+(n-16)*3;
+        }
+        return 8+(8*2)+(8*3)+(n-24)*4;
+    }
+}
