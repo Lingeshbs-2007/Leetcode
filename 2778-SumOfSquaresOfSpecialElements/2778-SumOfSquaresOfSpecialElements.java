8b4f6258fc858ca17b42f325110ba5f3bb1cfd80
Lingeshbs-2007 <lingeshbs2007@gmail.com>
2026-09-11 09:23:38 +0530

    Create 2778-SumOfSquaresOfSpecialElements

---
+++ 2778-SumOfSquaresOfSpecialElements/2778-SumOfSquaresOfSpecialElements.java
@@ -0,0 +1,13 @@
+// Last updated: 9/11/2026, 9:23:35 AM
+class Solution {
+    public int sumOfSquares(int[] nums) {
+         int n = nums.length;
+         int sum = 0 ;
+         for( int i = 1 ; i <= n ; i++){
+              if( n % i == 0){
+                   sum += nums[i-1] * nums[i-1];
+              }
+         }
+         return sum;
+    }
+}
