accacc3de9dd67f7a701598c82de26b214e8156e
Lingeshbs-2007 <lingeshbs2007@gmail.com>
2026-09-11 09:24:21 +0530

    Create 2481-MinimumCutsToDivideACircle

---
+++ 2481-MinimumCutsToDivideACircle/2481-MinimumCutsToDivideACircle.java
@@ -0,0 +1,11 @@
+// Last updated: 9/11/2026, 9:24:19 AM
+class Solution {
+    public int numberOfCuts(int n) {
+        if( n == 1)
+            return 0;
+        else if ( n % 2 == 0)
+            return n/2;
+        else
+            return n; 
+    }
+}
