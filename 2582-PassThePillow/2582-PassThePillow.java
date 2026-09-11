4249a5cba185d3714ee7c93e31e5d779d491dd88
Lingeshbs-2007 <lingeshbs2007@gmail.com>
2026-09-11 09:24:10 +0530

    Create 2582-PassThePillow

---
+++ 2582-PassThePillow/2582-PassThePillow.java
@@ -0,0 +1,8 @@
+// Last updated: 9/11/2026, 9:24:07 AM
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle = time / (n -1);
+        int rs = time % (n - 1);
+        return cycle % 2 == 0 ? rs+1 : n - rs;
+    }
+}
