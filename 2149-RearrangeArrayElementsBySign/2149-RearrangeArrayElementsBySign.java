a7e51927e9ebd3fa9b86126c697ebb43ea50fc5e
Lingeshbs-2007 <lingeshbs2007@gmail.com>
2026-09-11 09:24:47 +0530

    Create 2149-RearrangeArrayElementsBySign

---
+++ 2149-RearrangeArrayElementsBySign/2149-RearrangeArrayElementsBySign.java
@@ -0,0 +1,19 @@
+// Last updated: 9/11/2026, 9:24:44 AM
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int even = 0;
+        int odd = 1;
+        int res[] = new int[nums.length];
+        for(int num : nums){
+            if( num > 0){
+                res[even] = num;
+                even += 2;
+            }
+            else{
+                res[odd] = num;
+                odd += 2;
+            }
+        }
+        return res;
+    }
+}
